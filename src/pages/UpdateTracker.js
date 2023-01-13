818efad610e2142e4ae01e53a05e10523dba710a
Thomas J. Vu <thomasjvu@gmail.com>
2023-01-13 01:33:08 -0500

    Update button components

--- src/pages/UpdateTracker.js
+++ src/pages/UpdateTracker.js
@@ -1,12 +1,12 @@
 import TrackerDetails from '../components/TrackerDetails'
 import UpdateTrackerForm from '../components/UpdateTrackerForm'
 
-const UpdateTracker = (trackerCard) => {
+const UpdateTracker = ({tracker}) => {
     return (
         <>
             <h1>Update Bodyweight Tracker</h1>
             <UpdateTrackerForm />
-            {/* <TrackerDetails tracker={trackerCard}/> */}
+            {/*<TrackerDetails tracker={tracker}/>*/}
         </>
     )
 }
